81b0267305b0988f46e9e30fa321d026f12ccf54
Abhilash <abhilashshettigar@gmail.com>
2019-11-22 13:27:04 +0530

    Fixing Momemt Formats

--- reportToSlack.js
+++ reportToSlack.js
@@ -11,10 +11,12 @@ const slack = new SlackWebhook(process.env.SLACK_WEBHOOK_KEY);
 let title
 let stats
 
-const exportPath = './reports/index-'+moment().format("ddd-MMM-Do-YYYY-h-mm-ss-a")+'.html';
-var res = exportPath.slice(10, 44);
-
+const exportPath = './reports/index-'+moment().format("ddd-MMM-DD-YYYY-hh-mm-ss-a")+'.html';
+var res = exportPath.slice(10, 43);
 let s3ObjectURL = `https://newmanreports.s3.ap-south-1.amazonaws.com/${res}.html`;
+console.log(res)
+console.log(s3ObjectURL)
+console.log(exportPath)
 
 newman.run({
   collection: require('./tests/PostmanEcho.postman_collection.json'), //Add Postman collection path here
